b7029b80880582e61a73e13449e4873b879ea120
yxiangyu <1271380537@qq.com>
2023-04-22 16:07:30 +0800

    2023-4-22

--- leetcode/lc69. x 的平方根.cpp
+++ leetcode/lc69. x 的平方根.cpp
@@ -1,20 +1,22 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        double l=0,r=x;
-        if(x==1) return 1;
-        else if(x<1)
+        double l = 0, r = x;
+        if (x == 1) return 1;
+        else if (x < 1)
         {
-            l=x,r=1;
+            l = x, r = 1;
         }
-        
-        while((r-l)>=1e-8)
+
+        while ((r - l) >= 1e-8)
         {
-            double mid=(l+r)/2;
-            if(mid*mid>x)
-                r=mid;
+            double mid = (l + r) / 2;
+            if (fabs(mid * mid - x)<0.001)
+                return mid;
+            else if(mid* mid > x)
+                r = mid;
             else
-                l=mid;
+                l = mid;
             //cout<<l<<" "<<r<<endl;
         }
         //cout<<l<<endl;
